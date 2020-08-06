890bb0f6255b7671952bbcb1a29b9ba360add306
Rafael Costa <Rafaelbvj@gmail.com>
2020-08-06 17:46:51 -0300

    Add files via upload

--- image.hpp
+++ image.hpp
@@ -5,6 +5,10 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+
+#define FILE_INFO
+
+
 using namespace std;
 
 typedef struct bmp
@@ -38,28 +42,25 @@ private:
     M_BITMAP b;
     Pixel **pixels;
     char *filepath;
-    PE output;
-    PE *input;
 
-    long double result_calc;
-    map <long double,int> id_t;
-    vector <long double> dif_obtido;
 
-    void pixel_aloc(Pixel ***p, unsigned int col, unsigned int row)
+    void **pixel_alloc(unsigned int sizef, unsigned int col, unsigned int row)
     {
-        *p = (Pixel**)malloc(sizeof(Pixel*)*col);
+        void **p;
+        p = (void**)malloc(sizeof(void*)*col);
         for(unsigned int i=0; i<col; i++)
         {
-            (*p)[i] = (Pixel*)malloc(sizeof(Pixel)*row);
+            p[i] = malloc(sizef*row);
         }
+        return (void**)p;
     }
-    void pixel_free(Pixel ***p, int row)
+    void pixel_free(void **p, int row)
     {
         for(unsigned int i=0; i<row; i++)
         {
-            free( (*p)[i]);
+            free( p[i]);
         }
-        free(*p);
+        free(p);
 
     }
 public:
@@ -73,31 +74,33 @@ public:
     }
     int open_file(char *path)
     {
-        filepath = path;
         FILE *file = fopen(path,"rb");
         if(file == NULL)
         {
-            cout<<"Erro ao abrir o arquivo!"<<endl;
+            cerr<<"Erro ao abrir o arquivo!"<<endl;
             return errno;
         }
+        filepath = path;
         if(pixels !=NULL)
         {
-            pixel_free(&pixels,b.biHeight);
+            pixel_free((void**)pixels,b.biHeight);
         }
+
         char id[3] = {0,0,0};
         fread(id, sizeof(char),2, file);
         fread(&b, sizeof(M_BITMAP),1, file);
-        if(strcmp(id,"BM") !=0 )
+        if( strcmp(id,"BM") !=0 )
         {
-            cout<<"Arquivo não é BITMAP!"<<endl;
+            cerr<<"Arquivo nï¿½o ï¿½ BITMAP!"<<endl;
             return -1;
         }
+        #ifdef FILE_INFO
         cout<<"File:"<<path<<endl;
         cout<<"File size:"<<b.bfSize<<endl;
         cout<<"Image height:"<<b.biHeight<<endl;
         cout<<"Image width:"<<b.biWidth<<endl;
-
-        pixel_aloc(&pixels, b.biHeight, b.biWidth);
+        #endif
+        pixels = (Pixel**) pixel_alloc(sizeof(Pixel),b.biHeight, b.biWidth);
         int npad;
         for(npad =0; ((b.biWidth)*3+npad) % 4 !=0 ; npad++) {}
         char *pad = new char[npad];
@@ -111,153 +114,11 @@ public:
 
     }
 
-    int identify(int id)
-    {
-        const int n_elements = 5;
-        for(unsigned int u=0; u<n_elements; u++)
-        {
-            for(unsigned int i=0,t =0; i<b.biHeight; i++)
-            {
-                for(unsigned int j=0; j<b.biWidth; j++, t++)
-                {
-                    if(pixels[i][j].B==1){
-                        input[u].pe[t]->my_value_f = j;
-                    }
-                    else{
-                        input[u].pe[t]->my_value_f = 1;
-                    }
-                }
-
-            }
-            input[u].calc_value(0);
-
-        }
-
-        output.calc_value(0);
-        dif_obtido.push_back(fabs(output.my_value_f-result_calc));
-        id_t.insert(make_pair(*(dif_obtido.end()-1),id));
-
-        cout<<"Identificado:"<<output.my_value_f<<endl;
-    }
-
-    int give_min(){
-        sort(dif_obtido.begin(),dif_obtido.end());
-        return id_t[dif_obtido[0]];
-    }
-
-    int create_layer()
-    {
-        const int n_elements = 5;          //Número de elementos na "hidden layer"
-        unsigned int sz = b.biHeight*b.biWidth*n_elements;
-        unsigned int szwh = b.biHeight*b.biWidth;
-        input = new PE[szwh];
-        PE *setpe = new PE[sz];
-        for(unsigned int u=0,k=0; u<n_elements; u++)
-        {
-            for(unsigned int i=0; i<b.biHeight; i++)
-            {
-                for(unsigned int j=0; j<b.biWidth; j++, k++)
-                {
-                    //Criterio de entrada (Pixel == 1 "escuro" ) -> armazena X (width)
-                    if(pixels[i][j].B==1){
-                        setpe[k].my_value_f = j;
-                    }
-                    else{
-                        setpe[k].my_value_f = 1;
-                    }
-                    input[u].connect_pe(&setpe[k],input[u].generate_weight()/10000.0f); //Peso diminuido para evitar saturação da funcao de ativaçao
-                }
-
-            }
-            input[u].calc_value(0);
-        }
-        long double result = 0.7;               //Resultado esperado sempre deve ser <1, por causa da funcao sigmoid.
-
-        long double step = fastest_step_size(input[0].pe);
-        cout<<"Step size:"<<step<<endl;
-        for(unsigned int i=0; i<n_elements; i++)
-        {
-            output.connect_pe(&input[i], output.generate_weight());
-        }
-
-        vector <PE> outputs;
-
-        output.calc_value(0);
-        for(int epoc =0 ; epoc<50; epoc++)
-        {
-
-            for(unsigned int i=0; i<n_elements; i++)
-            {
-                output.compare_output(i, result, step);
-            }
-
-            outputs.push_back(output);
-
-            for(unsigned int i=0; i<n_elements; i++)
-            {
-
-                for(int e =0 ; e<szwh; e++)
-                {
-                    compare_forward(input[i],outputs,e,step);
-                }
-            }
-
-            for(unsigned int e=0; e<n_elements; e++)
-            {
-                input[e].calc_value(0);
-            }
-
-            outputs.clear();
-
-            output.calc_value(0);
-            cout<<"Epoca:"<<epoc+1<<endl;
-            cout<<output.my_value_f<<endl;
-        }
-        result_calc = output.my_value_f;
-
-
-    }
-    //only two colors
-    int set_binary_pixel()
-    {
-        if(pixels == NULL)
-        {
-            return 0;
-        }
-
-        for(unsigned int i=0; i<b.biHeight; i++)
-        {
-            for(unsigned int j=0; j<b.biWidth; j++)
-            {
-                if(pixels[i][j].B != 0xFF ||pixels[i][j].G != 0xFF||pixels[i][j].R != 0xFF)
-                {
-                    pixels[i][j].B  = 1;
-                    pixels[i][j].G  = 1;
-                    pixels[i][j].R  = 1;
-                }
-            }
-        }
-        int npad;
-        for(npad =0; ((b.biWidth)*3+npad) % 4 !=0 ; npad++) {}
-        char *pad = new char[npad];
-        FILE *file = fopen("teste2.bmp","wb");  //replace filepath
-        char id[3] = {'B','M'};
-        fwrite(id,1,2,file);
-        fwrite(&b,sizeof(M_BITMAP),1,file);
-        for(unsigned int i=0; i<b.biHeight; i++)
-        {
-            fwrite(pixels[i],sizeof(Pixel), b.biWidth,file);
-            fwrite(pad,npad, 1,file);
-        }
-        fclose(file);
-        delete []pad;
-
-    }
-
     ~Image()
     {
-        pixel_free(&pixels,b.biHeight);
+        pixel_free((void**)pixels,b.biHeight);
     }
 
+     template <typename> friend class Matrix;
 };
 #endif // IMAGE_H
--- perceptron.hpp
+++ perceptron.hpp
@@ -4,68 +4,136 @@
 #include <time.h>
 #include <vector>
 #include <math.h>
+#include <iostream>
 
+using namespace std;
 
 //Sigmoid as an activation function
 #define FUNC_ATIVA_LOG(a) 1/(1+exp(-a))
 #define FUNC_ATIVA_LOG_DER(a) FUNC_ATIVA_LOG(a)*(1-FUNC_ATIVA_LOG(a))
 #define FUNC_ATIVA_LOG_DER_F(a) a*(1-a)
 
-//RLU as an activation function
-#define FUNC_ATIVA_RLU(a)       ((a>0)?a:0)
-#define FUNC_ATIVA_RLU_DER(a)   ((a>0)?1:0)
-#define FUNC_ATIVA_RLU_DER_F(a) ((a!=0)?1:0)
+//ReLU as an activation function
+#define FUNC_ATIVA_RLU(a)       ((a>=0)?a:(a*0.01f))
+#define FUNC_ATIVA_RLU_DER(a)   ((a>=0)?1:0.01f)
+#define FUNC_ATIVA_RLU_DER_F(a) ((a>=0)?1:0.01f)
 
-//Tangente hiperbólico
+//Tangente hiperbï¿½lico
 #define FUNC_ATIVA_TANH(a)           tanh(a)
 #define FUNC_ATIVA_TANH_DER(a)       1-pow(tanh(a),2)
 #define FUNC_ATIVA_TANH_DER_F(a)     1-pow(a,2)
+
+
+typedef long double TYPE;
+typedef TYPE(*Func)(TYPE);
+
+typedef struct act_func{
+    Func activation_func;           //funcao de ativacao
+    Func activation_func_der;       //derivada
+    Func activation_func_der_f;     //derivada com o valor de retorno da funcao de ativacao como parametro
+}ActivationFunction;
+
 class PE
 {
 public:
 
-    std::vector <PE*> pe;                 //elementos de processos conectados
-    std::vector <long double> we ;        //pesos
-    std::vector <long double> we_old;     //pesos antes do ultimo ajuste
-    long double my_value;                 //valor elemento de processo, somatorio (Wi*Xi)
-    long double my_value_f;               //valor elemento de processo na funcao de ativacao
-    long double bi ;                      //bias coeficiente linear
+    std::vector <PE*> pe;          //elementos de processos conectados
+    std::vector <TYPE> we ;        //pesos
+    std::vector <TYPE> we_old;     //pesos antes do ultimo ajuste
+    TYPE my_value;                 //valor elemento de processo, somatorio (Wi*Xi)
+    long double my_value_f;        //valor elemento de processo na funcao de ativacao
+    TYPE bi ;                      //bias coeficiente linear
 
     // ef*fu*uw = erro instantaneo ou gradiente (dJ/dw)
-    long double ef;                       //Derivada do erro total em relacao a funçao separadora/ativacao
-    long double fu;                       //Derivada da funcao separadora em relacao ao valor do elemento de processo
-    long double uw;                       //Derivada do elemento de processo em relacao ao peso
+    TYPE ef;                       //Derivada do erro total em relacao a funï¿½ao separadora/ativacao
+    TYPE fu;                       //Derivada da funcao separadora em relacao ao valor do elemento de processo
+    TYPE uw;                       //Derivada do elemento de processo em relacao ao peso
+    ActivationFunction *s_func;
 
-    PE():my_value{0},my_value_f{0}
+    PE():my_value{0},my_value_f{0},s_func{nullptr}
     {
         srand(time(NULL));
     }
 
-    long double calc_value(long double b)
+    //Apenas para a camada de entrada
+    void set_activation_function(ActivationFunction *m){
+        s_func = m;
+    }
+
+    TYPE calc_value(TYPE b)
     {
         my_value = 0;
         bi = b;
-        for(unsigned int i=0; i<pe.size(); i++)
+        size_t size_pe = pe.size();
+        for(size_t i=0; i<size_pe; i++)
+        {
+            my_value+=pe[i]->my_value_f * we[i] ;
+        }
+        my_value += bi;
+        my_value_f = s_func->activation_func(my_value);
+        return my_value_f;
+    }
+
+    TYPE calc_value_SIGMOID(TYPE b)
+    {
+        my_value = 0;
+        bi = b;
+        size_t size_pe = pe.size();
+        for(size_t i=0; i<size_pe; i++)
+        {
+            my_value+=pe[i]->my_value_f * we[i] ;
+        }
+        my_value += bi;
+
+        my_value_f = FUNC_ATIVA_LOG(my_value);
+        return my_value_f;
+    }
+    TYPE calc_value_ReLU(TYPE b)
+    {
+        my_value = 0;
+        bi = b;
+        size_t size_pe = pe.size();
+        for(size_t i=0; i<size_pe; i++)
         {
             my_value+=pe[i]->my_value_f * we[i] ;
 
         }
         my_value += bi;
-        my_value_f = FUNC_ATIVA_LOG(my_value);
+
+        my_value_f = FUNC_ATIVA_RLU(my_value);
         return my_value_f;
     }
 
     float generate_weight(){
-        return (rand()%100)/float(100);
+        return (rand()%100)/100.0f;
+    }
+    bool compare_output(int index_weight,TYPE output, TYPE step_size)
+    {
+        
+        ef = -(output - my_value_f);
+        if(ef == 0)
+        {
+            //Resposta desejada, nao ï¿½ necessario ajustes
+            return true;
+        }
+        fu = s_func->activation_func_der_f(my_value_f);
+
+        uw = pe[index_weight]->my_value_f;
+
+        we_old[index_weight] = we[index_weight];
+
+        we[index_weight] -= step_size*ef*fu*uw;
+
+        return false;
+
     }
 
-
-    bool compare_output(int index_weight,long double output, long double step_size)
+    bool compare_output_SIGMOID(int index_weight,TYPE output, TYPE step_size)
     {
         ef = -(output - my_value_f);
         if(ef == 0)
         {
-            //Resposta desejada, nao é necessario ajustes
+            //Resposta desejada, nao ï¿½ necessario ajustes
             return true;
         }
         fu = FUNC_ATIVA_LOG_DER_F(my_value_f);
@@ -78,12 +146,36 @@ public:
 
         return false;
     }
-    bool connect_pe(PE *p, long double weight)
+    bool compare_output_ReLU(int index_weight,TYPE output, TYPE step_size)
+    {
+        ef = (my_value_f - output);        //Cost function - derivada do erro quadratico medio
+        if(ef == 0)
+        {
+            //Resposta desejada, nao ï¿½ necessario ajustes
+            return true;
+        }
+
+        fu = FUNC_ATIVA_RLU_DER_F(my_value_f);
+
+        uw = pe[index_weight]->my_value_f;
+
+        we_old[index_weight] = we[index_weight];
+
+        we[index_weight] -= step_size*ef*fu*uw;
+
+        return false;
+    }
+
+
+    bool connect_pe(PE *p, TYPE weight)
     {
         if(p == NULL)
         {
             return false;
         }
+        
+        s_func = p->s_func;
+        
         pe.push_back(p);
         we.push_back(weight);
         we_old.push_back(weight);
@@ -96,27 +188,30 @@ public:
         we.clear();
     }
 };
-long double largest_step_size(std::vector <PE*> input)
+TYPE largest_step_size(std::vector <PE*> input)
 {
-    long double lss =0;
-    for(unsigned int i=0; i<input.size(); i++)
+    TYPE lss =0;
+    size_t in_size = input.size();
+    for(size_t i=0; i<in_size; i++)
     {
         lss+=pow(input[i]->my_value_f, 2);
     }
     lss =(2*input.size())/lss;
     return lss;
 }
-long double fastest_step_size(std::vector <PE*> input)
+TYPE fastest_step_size(std::vector <PE*> input)
 {
     return largest_step_size(input)/2.0f;
+
 }
 
-bool compare_forward (PE& first, std::vector <PE> second_layer, int index_weight, long double step_size)
+
+bool compare_forward (PE& first, std::vector <PE> previous_layer, int index_weight, TYPE step_size)
 {
 
-    std::vector <PE>::iterator it = second_layer.begin();
+    std::vector <PE>::iterator it = previous_layer.begin();
     first.ef  = 0;
-    for(unsigned int c; it!=second_layer.end(); it++)
+    for(size_t c; it!=previous_layer.end(); it++)
     {
         for(c=0; c<(*it).pe.size(); c++)
         {
@@ -128,10 +223,79 @@ bool compare_forward (PE& first, std::vector <PE> second_layer, int index_weight
         }
         if(c==(*it).pe.size())
         {
-            //Elemento nao está conectado à camada
+            //Elemento nao estï¿½ conectado ï¿½ camada
             return false;
         }
-        first.ef += (*it).ef  *  (*it).we_old[c] * FUNC_ATIVA_LOG_DER_F((*it).my_value_f);
+        first.ef += (*it).ef  *  (*it).we_old[c] * first.s_func->activation_func_der_f((*it).my_value_f);
+
+    }
+
+    first.fu = first.s_func->activation_func_der_f(first.my_value_f);
+
+    first.uw = first.pe[index_weight]->my_value_f;
+
+    first.we_old[index_weight] = first.we[index_weight];
+
+    first.we[index_weight] -= first.ef*first.fu*first.uw*step_size;
+
+    return true;
+}
+
+bool compare_forward_ReLU(PE& first, std::vector <PE> previous_layer, int index_weight, TYPE step_size)
+{
+
+    std::vector <PE>::iterator it = previous_layer.begin();
+    first.ef  = 0;
+    for(size_t c; it!=previous_layer.end(); it++)
+    {
+        for(c=0; c<(*it).pe.size(); c++)
+        {
+            if((*it).pe[c] == &first)
+            {
+                break;
+            }
+
+        }
+        if(c==(*it).pe.size())
+        {
+            //Elemento nao estï¿½ conectado ï¿½ camada
+            return false;
+        }
+        first.ef += (*it).ef  *  (*it).we_old[c] * FUNC_ATIVA_RLU_DER_F((*it).my_value_f);
+    }
+
+    first.fu = FUNC_ATIVA_RLU_DER_F(first.my_value_f);
+
+    first.uw = first.pe[index_weight]->my_value_f;
+
+    first.we_old[index_weight] = first.we[index_weight];
+
+    first.we[index_weight] -= first.ef*first.fu*first.uw*step_size;
+
+    return true;
+}
+
+bool compare_forward_SIGMOID(PE& first, std::vector <PE> previous_layer, int index_weight, TYPE step_size)
+{
+
+    std::vector <PE>::iterator it = previous_layer.begin();
+    first.ef  = 0;
+    for(size_t c; it!=previous_layer.end(); it++)
+    {
+        for(c=0; c<(*it).pe.size(); c++)
+        {
+            if((*it).pe[c] == &first)
+            {
+                break;
+            }
+
+        }
+        if(c==(*it).pe.size())
+        {
+            //Elemento nao estï¿½ conectado ï¿½ camada
+            return false;
+        }
+        first.ef += (*it).ef  *  (*it).we_old[c]  *  FUNC_ATIVA_LOG_DER_F((*it).my_value_f);
     }
 
 
